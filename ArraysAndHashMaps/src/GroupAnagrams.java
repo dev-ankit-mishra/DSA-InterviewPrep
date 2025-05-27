74941d1049442c98cc6440e5494d3d5af6c102fb
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-05-27 10:45:39 +0530

    Group Anagrams [LeetCode-49]

---
+++ ArraysAndHashMaps/src/GroupAnagrams.java
@@ -0,0 +1,25 @@
+//Problem Link-https://leetcode.com/problems/group-anagrams/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map=new HashMap<>();
+
+        for(String word: strs){
+            char[] charArr=word.toCharArray();
+            Arrays.sort(charArr);
+            String str=new String(charArr);
+            if(!map.containsKey(str)){
+                map.put(str,new ArrayList<>());
+
+            }
+            map.get(str).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
