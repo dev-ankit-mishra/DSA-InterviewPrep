06ef899f886f0914781998e0fc5204a2c137b8fd
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-05-30 21:35:55 +0530

    Happy Number  [LeetCode-202]

--- ArraysAndHashMaps/src/HappyNumber.java
+++ ArraysAndHashMaps/src/HappyNumber.java
@@ -1,5 +1,27 @@
-//Problem Link-
+//Problem Link-https://leetcode.com/problems/happy-number/?envType=study-plan-v2&envId=top-interview-150
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class HappyNumber {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
 
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSquareSum(n);
+        }
+
+        return n == 1;
+    }
+
+    private int getSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
 }
