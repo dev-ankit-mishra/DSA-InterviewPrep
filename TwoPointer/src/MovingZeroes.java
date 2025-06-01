621a3c2cc62bdedc24f0323ba5397b2fe0eaebdf
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-01 11:16:11 +0530

    Move Zeroes  [LeetCode-283]

---
+++ TwoPointer/src/MovingZeroes.java
@@ -0,0 +1,21 @@
+//Problem Link-https://leetcode.com/problems/move-zeroes/
+
+public class MovingZeroes {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int i=0;
+
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0){
+                swap(nums,i,j);
+                i++;
+            }
+        }
+
+    }
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
