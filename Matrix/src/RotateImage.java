9bd6350383e858e040ca4a6b697e4f55e9c75db3
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-01 10:16:49 +0530

    Rotate Image  [LeetCode-48]

--- Matrix/src/RotateImage.java
+++ Matrix/src/RotateImage.java
@@ -3,7 +3,7 @@
 public class RotateImage {
     //Method-1
 
-    public void rotate(int[][] matrix) {
+    public void rotate1(int[][] matrix) {
         int r=matrix.length;
         int c=matrix[0].length;
         int[][] copy=new int[r][];
@@ -18,5 +18,29 @@ public class RotateImage {
     }
 
     //Method-2
+
+    public void rotate2(int[][] matrix) {
+        int r=matrix.length;
+        int c=matrix[0].length;
+
+        //transpose
+        for(int i=0;i<r;i++){
+            for(int j=i;j<c;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        //reverse
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][c-j-1];
+                matrix[i][c-j-1]=temp;
+            }
+        }
+
+    }
 
 }
