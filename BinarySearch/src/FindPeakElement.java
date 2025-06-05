f502c8ff27bc786dc8702dd024b5f7527ae491ed
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-05 23:40:35 +0530

    Find Peak Element  [LeetCode-162]

--- BinarySearch/src/FindPeakElement.java
+++ BinarySearch/src/FindPeakElement.java
@@ -1,5 +1,24 @@
-//Problem Link-
+//Problem Link-https://leetcode.com/problems/find-peak-element/description/?envType=study-plan-v2&envId=top-interview-150
 
 public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int lo=0;
+        int hi=n-1;
+
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]>nums[mid+1]){
+                hi=mid;
+            }else{
+                lo=mid+1;
+            }
+        }
+
+        return hi;
+
+
+    }
 
 }
