9d13acddbaf3b60bdca18efeafa0b754ae480024
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-05 22:59:36 +0530

    Search a 2D Matrix  [LeetCode-74]

--- BinarySearch/src/SearchA2DMatrix.java
+++ BinarySearch/src/SearchA2DMatrix.java
@@ -31,5 +31,20 @@ public class SearchA2DMatrix {
         return false;
     }
     //Method-2
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r=0;
+        int c=matrix[0].length-1;
+
+        while(r<matrix.length && c>=0){
+            if(matrix[r][c]==target){
+                return true;
+            }else if(matrix[r][c]<target){
+                r++;
+            }else{
+                c--;
+            }
+        }
+        return false;
+    }
 
 }
