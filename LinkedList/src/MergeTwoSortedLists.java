5d0fed50d7601831184b6e456a5495b538f49bc6
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-07 10:54:48 +0530

    Merge Two Sorted Lists [LeetCode-21]

---
+++ LinkedList/src/MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+//Problem Link-
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node=new ListNode(-1,null);
+        ListNode temp1=list1;
+        ListNode temp2=list2;
+        ListNode temp3=node;
+
+        while(temp1!=null && temp2!=null){
+            if(temp1.val<=temp2.val){
+                temp3.next=temp1;
+                temp1=temp1.next;
+                temp3=temp3.next;
+
+            }else{
+                temp3.next=temp2;
+                temp2=temp2.next;
+                temp3=temp3.next;
+
+
+            }
+        }
+        if(temp1!=null){
+            temp3.next=temp1;
+
+        }else{
+            temp3.next=temp2;
+        }
+
+        return node.next;
+    }
+}
