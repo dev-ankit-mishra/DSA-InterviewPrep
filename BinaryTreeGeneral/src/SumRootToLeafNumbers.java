8229f06089e4cb72a92e73d5b4cae07e3e4f030d
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-10 10:26:39 +0530

    Sum Root to Leaf Numbers[LeetCode-129]

--- BinaryTreeGeneral/src/SumRootToLeafNumbers.java
+++ BinaryTreeGeneral/src/SumRootToLeafNumbers.java
@@ -36,6 +36,20 @@ public class SumRootToLeafNumbers {
     }
 
     //Method-2
+    public int sumNumbers2(TreeNode root) {
+        return dfs(root,0);
+    }
+    public int dfs(TreeNode root,int sum){
+        if(root==null){
+            return 0;
+        }
+        sum=sum*10+root.val;
 
+        if(root.left==null && root.right==null){
+            return sum;
+        }
+        return dfs(root.left,sum) + dfs(root.right,sum);
+
+    }
 
 }
