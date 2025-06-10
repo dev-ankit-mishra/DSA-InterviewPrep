92f4030a16697ed8317ee1cfbd1f9ed3b24798ac
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-10 10:20:54 +0530

    Sum Root to Leaf Numbers[LeetCode-129]

---
+++ BinaryTreeGeneral/src/SumRootToLeafNumbers.java
@@ -0,0 +1,41 @@
+//Problem Link-https://leetcode.com/problems/sum-root-to-leaf-numbers/description/?envType=study-plan-v2&envId=top-interview-150
+
+import java.util.Stack;
+
+public class SumRootToLeafNumbers {
+    //Method-1
+    class Pair{
+        TreeNode top;
+        int num;
+        Pair(TreeNode top,int num){
+            this.top=top;
+            this.num=num;
+        }
+    }
+    public int sumNumbers(TreeNode root) {
+        Stack<Pair> st=new Stack<>();
+        if(root!=null) st.push(new Pair(root,root.val));
+
+        int sum=0;
+        while(!st.isEmpty()){
+            Pair pair=st.pop();
+            TreeNode top=pair.top;
+            int num=pair.num;
+
+            if(top.left==null && top.right==null){
+                sum+=num;
+            }
+            if(top.right!=null){
+                st.push(new Pair(top.right,num*10+top.right.val));
+            }
+            if(top.left!=null){
+                st.push(new Pair(top.left,num*10+top.left.val));
+            }
+        }
+        return sum;
+    }
+
+    //Method-2
+
+
+}
