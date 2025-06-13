73858a49d5d7cb2a9b47c28a2ff0cf450605bc3c
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-13 11:24:56 +0530

    Binary Tree Right Side View[LeetCode-199]

--- BinaryTreeBFS/src/BinaryTreeRightSideView.java
+++ BinaryTreeBFS/src/BinaryTreeRightSideView.java
@@ -1,2 +1,30 @@
+//Problem Link-https://leetcode.com/problems/binary-tree-right-side-view/description/?envType=study-plan-v2&envId=top-interview-150
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root!=null) q.add(root);
+
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode head=q.poll();
+                if(i==size-1){
+                    ans.add(head.val);
+                }
+                if(head.left!=null) q.add(head.left);
+                if(head.right!=null) q.add(head.right);
+
+            }
+        }
+
+        return ans;
+
+    }
 }
