2a5e1370405ead7874451bf42caecb23c01ca8b0
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-13 12:00:09 +0530

    Validate Binary Search Tree [LeetCode-98]

--- BinarySearchTree/src/ValidateBinarySearchTree.java
+++ BinarySearchTree/src/ValidateBinarySearchTree.java
@@ -1,2 +1,14 @@
+//Problem Link-https://leetcode.com/problems/validate-binary-search-tree/description/?envType=study-plan-v2&envId=top-interview-150
+
 public class ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    public boolean helper(TreeNode root,long min,long max){
+        if(root==null) return true;
+
+        if(root.val<=min || root.val>=max) return false;
+
+        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+    }
 }
