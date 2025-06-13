dc7254e458ee7b97b6c8ded39d8a16d118f2abd6
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-13 12:01:56 +0530

    Validate Binary Search Tree [LeetCode-98]

--- BinarySearchTree/src/ValidateBinarySearchTree.java
+++ BinarySearchTree/src/ValidateBinarySearchTree.java
@@ -1,14 +1,45 @@
 //Problem Link-https://leetcode.com/problems/validate-binary-search-tree/description/?envType=study-plan-v2&envId=top-interview-150
 
+import java.util.Stack;
+
 public class ValidateBinarySearchTree {
+
+    //Method-1
     public boolean isValidBST(TreeNode root) {
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    public boolean helper(TreeNode root,long min,long max){
-        if(root==null) return true;
 
-        if(root.val<=min || root.val>=max) return false;
+    public boolean helper(TreeNode root, long min, long max) {
+        if (root == null) return true;
 
-        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+        if (root.val <= min || root.val >= max) return false;
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+
+    //Methode-2
+
+    public boolean isValidBST2(TreeNode root) {
+
+        Stack<TreeNode> st = new Stack<>();
+        long prev = Long.MIN_VALUE;
+        TreeNode curr = root;
+        while (curr != null || !st.isEmpty()) {
+
+            while (curr != null) {
+                st.push(curr);
+                curr = curr.left;
+            }
+            curr = st.pop();
+
+            if (prev >= curr.val) {
+                return false;
+            }
+
+            prev = curr.val;
+
+            curr = curr.right;
+        }
+        return true;
     }
 }
